0066ee7ef3413e778cee4203d1f808dc25a5914a
Дмитрий <na3gul2731@mail.ru>
2022-11-17 21:26:30 +0300

    Add homework 1

--- homework_01/main.py
+++ homework_01/main.py
@@ -7,15 +7,13 @@
 def power_numbers(*nums):
    return(i ** 2 for i in nums)
 
-power_numbers(1, 2, 5, 7)
-
 
 # filter types
 ODD = "odd"
 EVEN = "even"
 PRIME = "prime"
 
-def is_prime(a):
+def is_prime(*a):
     if a == 1:
         return False
     else:
@@ -26,7 +24,7 @@ def is_prime(a):
 
 
 
-def filter_numbers(a, b):
+def filter_numbers(*a, b):
     if b == ODD:
         return (filter(lambda x: x % 2, a))
     elif b == EVEN:
